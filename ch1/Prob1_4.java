6b5bb77140b3e4c03e28cb91b407239658b3268d
kwilsonO <kylewilson52@gmail.com>
2016-04-01 09:03:31 -0700

    prob 1.4

--- ch1/Prob1_4.java
+++ ch1/Prob1_4.java
@@ -3,22 +3,23 @@ import java.util.*;
 //uppercase change 'a' to 'A'
 //full Ascii use 256 for charSetSize
 
-public class Prob1_2{
+public class Prob1_4{
 
 	public static void main(String[] Args){
 
 		int charSetSize = 26;
-		String str1 = "fubar";	
-		String str2 = "xxfar";
+		String str1 = "xacecar";	
+		String str2 = "caxrace";
 	
 		BitSet bits1 = new BitSet(charSetSize);
 		BitSet bits2 = new BitSet(charSetSize);
-
+		BitSet pali = new BitSet(charSetSize);
 			
 
 		for(int x = 0; x < str1.length(); x++){
 			bits1.set(str1.charAt(x) - 'a');
 			bits2.set(str2.charAt(x) - 'a');
+			pali.flip(str1.charAt(x) - 'a');
 		}	
 
 	//	System.out.println("Bitset1" + bits1);
@@ -33,9 +34,14 @@ public class Prob1_2{
 			
 			System.out.println("Not permutation");
 		} else {
+			
+			if(pali.cardinality() == 1) {
 
-			System.out.println("Permutation");
+				System.out.println("Perm of pali");
 
+			} else {
+				System.out.println("Perm but not pali");
+			}
 		}
 
 	}
